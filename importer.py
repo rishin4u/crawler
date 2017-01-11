d0da6228f191fda490d153d9370eb41280e2afcd
rishinbabu <rishin.s.babu.cet@gmail.com>
2017-01-11 17:24:21 +0530

    Changed importer

--- importer.py
+++ importer.py
@@ -1,6 +1,6 @@
 import os, sys,urllib2,json,urllib,time
-lib_path = os.path.relpath('lib/beautifulsoup4-4.1.2')
-sys.path.append(lib_path)
+#lib_path = os.path.relpath('lib/beautifulsoup4-4.1.2')
+#sys.path.append(lib_path)
 from pymongo import MongoClient as Connection
 from urlparse import urlparse
 from bson import BSON
